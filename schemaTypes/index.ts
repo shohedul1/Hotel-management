9dc835085d48b299ad42a8ec484e3395841930aa
Your Name <you@example.com>
2024-03-29 02:58:44 -0700

    frist-comit

---
+++ schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import account from "./account";
+import booking from "./booking";
+import review from "./review";
+import user from "./user";
+import verificationToken from "./verificationToken";
+
+export const schemaTypes = [user,account,booking,review,verificationToken];
